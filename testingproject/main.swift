7dbd5d087c42026e84b4e74ca4cceb407e0e1f5b
Paul Styslinger <pablitod123@gmail.com>
2014-09-02 20:48:17 -0400

    Initial Commit

---
+++ testingproject/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  testingproject
+//
+//  Created by Paul Styslinger on 9/2/14.
+//  Copyright (c) 2014 Paul Styslinger. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
